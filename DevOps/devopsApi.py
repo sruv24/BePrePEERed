a5228a2ddec44621cfad50662f2d84596e33cf73
sruthi2498 <vsruthi98@gmail.com>
2019-11-18 11:16:28 +0530

    removed stuff

--- DevOps/devopsApi.py
+++ DevOps/devopsApi.py
@@ -1,37 +1,8 @@
-from azure.devops.connection import Connection
-from msrest.authentication import BasicAuthentication
 import pprint
 import requests
 import json
 
 
-def getDevopsClient(personal_access_token,organization_url):
-    credentials = BasicAuthentication('', personal_access_token)
-    connection = Connection(base_url=organization_url, creds=credentials)
-    core_client = connection.clients.get_core_client()
-    return core_client
-
-def getAllProjects(core_client):
-    all_projects=[]
-    get_projects_response = core_client.get_projects()
-    index = 0
-    while get_projects_response is not None:
-        for project in get_projects_response.value:
-            all_projects.append({"id":project.id,"name":project.name,"url":project.url})
-            index += 1
-            #print(dict(project))
-        if get_projects_response.continuation_token is not None and get_projects_response.continuation_token != "":
-            # Get the next page of projects
-            get_projects_response = core_client.get_projects(continuation_token=get_projects_response.continuation_token)
-        else:
-            # All projects have been retrieved
-            get_projects_response = None
-    #print(core_client.get_project(1))
-
-    return all_projects
-
-def getProject(core_client,project_id):
-    return core_client.get_project(project_id)
 
 
 def makeGETRequest(url,cookie):
